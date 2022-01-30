8d7ee434b2f9f89cea9f85b0dc03e92beb79af96
Willie Lei <wlei@macbook-pro-wlei.local>
2022-01-30 18:18:45 -0500

    Process data

--- mdr_join_device.py
+++ mdr_join_device.py
@@ -1,80 +1,82 @@
 from collections import defaultdict
+from os import listdir
+from os.path import isfile, join
 
-joined_data = defaultdict(list)
 
 # Open the file
-MDR_FILENAME = 'end100mdrfoiThru2020.txt'
-DEVICE_FILENAMES = [
-	('end100device2020.txt', False)
+MDR_FILENAME = 'data/mdrfoiThru2020.txt'
+DEVICE_FILENAMES = [f for f in listdir('data/device-data') if isfile(join('data/device-data', f))]
+DEVICE_FILENAMES.sort()
+assert len(DEVICE_FILENAMES) == 23, "Missing device file"
+
+DEVICE_COLUMNS_OF_INTEREST = [
+	# In device files
+	'MDR_REPORT_KEY',
+	'DEVICE_REPORT_PRODUCT_CODE',
+	'DATE_RECEIVED',
+	'BRAND_NAME',
+	'GENERIC_NAME',
+	'MANUFACTURER_D_NAME',
 ]
-MDR_HEADER_FILENAME = 'headingmdrfoiThru2020.txt'
-DEVICE_HEADER_FILENAME = 'headingdevice.txt'
-# DEVICE_NAMES = set([
-# 	'STENT, CORONARY',
-# 	'CORONARY DRUG-ELUTING STENT',
-# 	'ABSORBABLE CORONARY DRUG-ELUTING STENT',
-# 	'CORONARY COVERED STENT'
-# ])
+DEVICE_REPORT_PRODUCT_CODE_OUTPUT_COLUMN = DEVICE_COLUMNS_OF_INTEREST.index('DEVICE_REPORT_PRODUCT_CODE')
+
+MDR_COLUMNS_OF_INTEREST = [
+	# In MDR file
+	'MDR_REPORT_KEY',
+	'DATE_RECEIVED',
+	'DATE_REPORT',
+	'DATE_OF_EVENT',
+	'DEVICE_DATE_OF_MANUFACTURE',
+	'EVENT_TYPE',
+]
+joined_data = defaultdict(lambda: [None,[]])
+
 DEVICE_REPORT_PRODUCT_CODES = set([
 	'MAF',
 	'NIQ',
 	'PNY',
 	'NIV'
 ])
-ROWS_OUTPUT_FILE = 'rows_output.txt'
-GROUPS_OUTPUT_FILE = 'groups_output.txt'
-
-with open(MDR_HEADER_FILENAME) as header_file:
-	for row in header_file:
-		mdr_column_names = row.strip().split('|')
-		event_type_column = mdr_column_names.index('EVENT_TYPE')
-		break
-
-with open(DEVICE_HEADER_FILENAME) as header_file:
-	for row in header_file:
-		device_column_names = row.strip().split('|')
-		product_code_column = device_column_names.index('DEVICE_REPORT_PRODUCT_CODE')
-		date_received_column = device_column_names.index('DATE_RECEIVED')
-
-		break
-
-# print(mdr_column_names)
-# print(device_column_names)
-
+ROWS_OUTPUT_FILE = 'filtered_rows.txt'
+GROUPS_OUTPUT_FILE = 'count_by_group.txt'
 
 # Iterate through all device files
 num_device_mismatch = 0
 num_device_match = 0
 
-for device_filename in DEVICE_FILENAMES:
-	filename, has_header = device_filename
-	print('parsing', filename)
-	with open(filename) as devicefile:
-		read_header = False
+for filename in DEVICE_FILENAMES:
+	print('Parsing', filename)
+	with open(f'data/device-data/{filename}', encoding="latin-1") as devicefile:
 		num_device_lines_parsed = 0
+		read_header = False
 
+		# Go through each line in the device file
 		for line in devicefile:
-			if has_header and not read_header:
-				read_header = True
-				continue
 			num_device_lines_parsed += 1
+			row = line.strip().split('|')
+
 			if num_device_lines_parsed % 100000 == 0:
 				print('num_device_lines_parsed =', num_device_lines_parsed)
-			row = line.strip().split('|')
+
+			if not read_header:
+				read_header = True
+				device_column_indexes = [row.index(c) for c in DEVICE_COLUMNS_OF_INTEREST]
+				device_report_product_code_column = device_column_indexes[DEVICE_REPORT_PRODUCT_CODE_OUTPUT_COLUMN]
+				continue
+
 			mdr_report_key = row[0]
 
-			if mdr_report_key in joined_data:
-				print(mdr_report_key, 'mdr_report_key appears twice in', filename)
-				continue
-
-			if product_code_column >= len(row):
+			# If the DEVICE_REPORT_PRODUCT_CODE column doesn't exist for the row
+			if device_report_product_code_column >= len(row):
 				num_device_mismatch += 1
 				continue
-			device = row[product_code_column]
 
+			device = row[device_report_product_code_column]
+
+			# We only want rows where the device is in the set DEVICE_REPORT_PRODUCT_CODES
 			if device in DEVICE_REPORT_PRODUCT_CODES:
-				# mdr_report_keys in both
-				joined_data[mdr_report_key].append(row)
+				selected_columns = [row[i] if i < len(row) else None for i in device_column_indexes]
+				joined_data[mdr_report_key][1].append(selected_columns)
 				num_device_match += 1
 			else:
 				num_device_mismatch += 1
@@ -84,67 +86,84 @@ print('num_device_mismatch =', num_device_mismatch)
 # Open the MDR file
 num_only_in_mdr = 0
 num_missing_date_received = 0
-with open(MDR_FILENAME) as mdrfile:
+
+with open(MDR_FILENAME, encoding="latin-1") as mdrfile:
 	# Iterate through each line of the file
 	num_mdr_lines_parsed = 0
-	
+	read_header = False
+
 	for line in mdrfile:
-		# >= Jan 1 2011
 		num_mdr_lines_parsed += 1
+		row = line.strip().split('|') # Split each row by the | symbol
 
 		if num_mdr_lines_parsed % 1000000 == 0:
 			print('num_mdr_lines_parsed =', num_mdr_lines_parsed)
-		row = line.strip().split('|') # Split each row by the | symbol
-		
-		# if date_received_column >= len(row):
-		# 	num_missing_date_received += 1
-		# 	continue
-		# if row[date_received_column] < '2011/01/01':
-		# 	continue
+
+		if not read_header:
+			read_header = True
+			mdr_column_indexes = [row.index(c) for c in MDR_COLUMNS_OF_INTEREST]
+			continue
 		
 		mdr_report_key = row[0] # Get the MDR Report Key (first column)
+
 		if mdr_report_key in joined_data:
-			if len(joined_data[mdr_report_key]) >= 2:
+			if joined_data[mdr_report_key][0]:
 				print(mdr_report_key, 'mdr_report_key appears twice in', mdrfile)
 			else:
-				joined_data[mdr_report_key].append(row) # Add the row to the end of the list
+				selected_columns = [row[i] if i < len(row) else '' for i in mdr_column_indexes]
+				joined_data[mdr_report_key][0] = selected_columns # Add the row to the end of the list
 		else:
 			num_only_in_mdr += 1
+
 print('num_only_in_mdr =', num_only_in_mdr)
 print('num_missing_date_received =', num_missing_date_received)
-
+''''
+mdr row device 1
+mdr row device 2
+...
+'''
 num_only_in_device = 0
 num_rows_written = 0
 
 groups = defaultdict(int)
 
 with open(ROWS_OUTPUT_FILE, 'w') as file:
-	file.write('|'.join(device_column_names + mdr_column_names))
+	file.write('|'.join(DEVICE_COLUMNS_OF_INTEREST + MDR_COLUMNS_OF_INTEREST))
 	file.write('\n')
+	date_of_event_index = MDR_COLUMNS_OF_INTEREST.index('DATE_OF_EVENT')
+	product_code_index = DEVICE_COLUMNS_OF_INTEREST.index('DEVICE_REPORT_PRODUCT_CODE')
+	event_type_index = MDR_COLUMNS_OF_INTEREST.index('EVENT_TYPE')
 
 	for mdr_report_key in joined_data:
-		rows = joined_data[mdr_report_key]
+		mdr_row, device_rows = joined_data[mdr_report_key]
 
-		if len(rows) == 1:
+		if mdr_row is None:
 			num_only_in_device += 1
 			continue
 
-		device_row, mdr_row = rows[:2]
-		num_rows_written += 1
-		year_month = device_row[date_received_column][:-3] if date_received_column < len(device_row) else 'NULL'
-		product_code = device_row[product_code_column] if product_code_column < len(device_row) else 'NULL'
-		event_type = mdr_row[event_type_column] if event_type_column < len(mdr_row) else 'NULL'
-		group = (year_month, product_code, event_type)
-		groups[group] += 1
+		date_of_event = mdr_row[date_of_event_index].split('/')
+		if len(date_of_event) == 3:
+			month, day, year = date_of_event
+			assert len(month) == 2
+			assert len(day) == 2
+			assert len(year) == 4
+		else:
+			month, day, year = 'Unknown', 'Unknown', 'Unknown'
 		
-
-		file.write('|'.join(rows[0] + rows[1]))
-		file.write('\n')
+		product_code = device_rows[0][product_code_index]
+		event_type = mdr_row[event_type_index]
+		group = (f'{year}/{month}', product_code, event_type)
+		groups[group] += len(device_rows)
+		
+		for device_row in device_rows:
+			num_rows_written += 1
+			file.write('|'.join(mdr_row + device_row))
+			file.write('\n')
 print('num_only_in_device =', num_only_in_device)
 print('num_rows_written =', num_rows_written)
 
 with open(GROUPS_OUTPUT_FILE, 'w') as file:
-	file.write('YEAR/MONTH|PRODUCT_CODE|EVENT_TYPE\n')
+	file.write('YEAR/MONTH|DEVICE_REPORT_PRODUCT_CODE|EVENT_TYPE\n')
 
 	for group in sorted(groups.keys()):
 		count = groups[group]
